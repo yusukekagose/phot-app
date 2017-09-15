f4cbdbde03312adbce65da3a3ae6b3c326552bb3
kagose yusuke <kagoseyusuke@kagose-no-MacBook-Air.local>
2017-09-15 10:38:09 +0900

    add sendgrid func

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,9 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'yusuke-phot-app.herokuapp.com', :protocol => 'https'}
+
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
