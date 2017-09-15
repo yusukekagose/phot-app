8a4224ce60cc94235c9ec9845f3475e181254efe
kagose yusuke <kagoseyusuke@kagose-no-MacBook-Air.local>
2017-09-16 00:18:27 +0900

    fix various things

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :confirmable,:recoverable, :rememberable, :trackable, :validatable
   has_one :payment
   accepts_nested_attributes_for :payment
+  has_many :images
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :images
   devise_for :users, :controller => { :registrations => 'registrations'}
   root 'welcome#index'
 
