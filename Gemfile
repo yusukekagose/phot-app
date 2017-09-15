c5e08ea4f4e1778943d0f8a24c919f8cdd62d5df
kagose yusuke <kagoseyusuke@kagose-no-MacBook-Air.local>
2017-09-15 14:08:12 +0900

    let user to user stripe payment

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'devise'
 gem "font-awesome-rails"
 gem 'twitter-bootstrap-rails'
 gem 'devise-bootstrap-views'
+gem 'stripe'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,:recoverable, :rememberable, :trackable, :validatable
+  has_one :payment
+  accepts_nested_attributes_for :payment
 end
